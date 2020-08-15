1d6922b5a91dde379beb0f28099eaf199e9a458a
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-08-16 01:11:47 +0530

    Fixed Issue in Register API

---
+++ addresses/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import *
+from accounts.serializers import UserMiniSerializer
+
+class AddressCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            'id',
+            'user',
+            'name',
+            'address_1',
+            'address_2',
+            'landmark',
+            'city',
+            'state',
+            'country',
+            'pincode',
+            'alt_phone',
+            'lat',
+            'lng',
+            'is_default',
+            'is_active',
+            'is_archived',
+            'created',
+            'updated'
+        ]
+
+    
+class AddressShowSerializer(serializers.ModelSerializer):
+    user = UserMiniSerializer(read_only=True)
+    class Meta:
+        model = Address
+        fields = [
+            'id',
+            'user',
+            'name',
+            'address_1',
+            'address_2',
+            'landmark',
+            'city',
+            'state',
+            'country',
+            'pincode',
+            'alt_phone',
+            'lat',
+            'lng',
+            'is_default',
+            'is_active',
+            'is_archived',
+            'created',
+            'updated'
+        ]
--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class CreateAddress(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Address.objects.all()
+    serializer_class = AddressCreateSerializer
+
+
+class ListAllAddresses(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Address.objects.all()
+    serializer_class = AddressShowSerializer
+    filterset_fields = [
+        'user',
+        'name',
+        'landmark',
+        'city',
+        'state',
+        'country',
+        'pincode',
+        'lat',
+        'lng',
+        'is_default',
+        'is_active',
+        'is_archived',
+    ]
+
+
+class UpdateAddress(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Address.objects.all()
+    serializer_class = AddressShowSerializer
+    lookup_field = 'id'
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
