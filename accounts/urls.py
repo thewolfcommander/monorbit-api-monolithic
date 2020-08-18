9ff51db10df36e36c0d40cb4172d30c8bedd73a2
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-08-18 16:41:52 +0530

    Fixed Bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register_view'),
     path('verify-mobile/', VerifyOTPView.as_view(), name='verify_mobile'),
     path('resend-mobile-otp/', ResendMobileVerifyOTPView.as_view(), name='resend_mobile_otp'),
-    path('forgot-pasword/', ForgotPasswordView.as_view()),
-    path('reset-pasword/', ResetPasswordView.as_view()),
+    path('forgot-password/', ForgotPasswordView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
 ]
