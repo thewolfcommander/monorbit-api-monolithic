199a9b5a4132b1287ba29fb9466d49122c8f62a9
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-08-25 13:17:52 +0530

    Deployment started

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add . && git commit -m "Deployment started"
 git push origin master
 git push heroku master
-heroku run python3 manage.py migrate --app monorbit-alpha
+# heroku run python3 manage.py migrate --app monorbit-alpha
 # heroku config:set MAILGUN_API_URL=https://api.mailgun.net/v3/mg.monorbit.com/messages --app monorbit-alpha
 # heroku config:set MAILGUN_API_KEY=f381a922d34b77e8f7eb2b300486473a-1df6ec32-6ccd846f --app monorbit-alpha
 # heroku config:set SMTP_PASSWORD=146a1b4981013213f38f226798eb990c-4d640632-babc1148 --app monorbit-alpha
