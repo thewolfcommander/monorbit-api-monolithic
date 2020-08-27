0523c54971c7e606ea9f514476a24b2cba0ca7c2
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-08-27 18:20:14 +0530

    Deployment started

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git push origin master
 git push heroku master
 heroku run python3 manage.py migrate --app monorbit-alpha
 
-heroku config:set SENTRY_DSN=https://d99af8b497ae447184234d7e41bd4c8b@o438843.ingest.sentry.io/5404510 --app monorbit-alpha
+# heroku config:set SENTRY_DSN=https://d99af8b497ae447184234d7e41bd4c8b@o438843.ingest.sentry.io/5404510 --app monorbit-alpha
