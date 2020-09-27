a9012193262814072fa88a0d748293a335fadeef
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-09-27 23:23:59 +0530

    Adding Logging

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add . && git commit -m "Adding Logging"
 git push origin v1.1
 git push heroku master
-# heroku run python3 manage.py migrate --app monorbit-alpha
+heroku run python3 manage.py migrate --app monorbit-alpha
 
 # heroku config:set  --app monorbit-alpha
 # heroku config:set RAZORPAY_API_KEY=rzp_test_x1OuprBYA5F1x0 --app monorbit-alpha
