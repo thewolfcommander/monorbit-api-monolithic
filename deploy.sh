9e782d9da0cb9c5e3eaa7a460f6a71ea8f5476a9
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-08 03:40:09 +0530

    Deploying on AWS EC2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add . && git commit -m "Deploying on AWS EC2"
 git push origin master
-# git push heroku master
-# heroku run python3 manage.py migrate --app monorbit-alpha
+git push heroku master
+heroku run python3 manage.py migrate --app monorbit-alpha
 
 # heroku config:set  --app monorbit-alpha
 # heroku config:set RAZORPAY_API_KEY=rzp_test_x1OuprBYA5F1x0 --app monorbit-alpha
