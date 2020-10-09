a81a2c7281dc19484b2a1051cc82a791895d0fb6
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-09 22:17:37 +0530

    Deploying on AWS EC2

--- dp.sh
+++ dp.sh
@@ -1,3 +1,3 @@
 git add . && git commit -m "Deploying on AWS EC2"
 git push origin master
-ssh -i "monorbit-alpha.pem" -t ubuntu@ec2-54-187-152-205.us-west-2.compute.amazonaws.com 'sudo sh ./deploy/update.sh; exec $SHELL'
+ssh -i "monorbit-alpha.pem" -t ubuntu@ec2-54-187-152-205.us-west-2.compute.amazonaws.com 
