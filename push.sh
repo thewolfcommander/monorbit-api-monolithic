e6271587a89e27d698cbeffff4efb50c54148019
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-11 20:54:44 +0530

    Deploying on AWS EC2

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 git add . && git commit -m "Deploying on AWS EC2"
-git push
+# git push
+git push --set-upstream origin v1.1.0
 # git push heroku master
 # heroku run python3 manage.py migrate --app monorbit-alpha
 
