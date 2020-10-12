df8f88337d4f089d9ba83a4b6782b115eb9bb7d9
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-12 17:50:29 +0530

    Added Image upload feature, connected RDS

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -12,33 +12,33 @@ apt-get update
 apt-get install -y python3-dev sqlite python3-pip supervisor nginx git
 
 # Create project directory
-# mkdir -p $PROJECT_BASE_PATH
-# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
-# Create virtual environment
-pip3 install virtualenv
-python3 -m virtualenv $PROJECT_BASE_PATH/env
+# # Create virtual environment
+# pip3 install virtualenv
+# python3 -m virtualenv $PROJECT_BASE_PATH/env
 
-# Install python packages
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
+# # Install python packages
+# $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+# $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 
-# Run migrations and collectstatic
-cd $PROJECT_BASE_PATH
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+# # Run migrations and collectstatic
+# cd $PROJECT_BASE_PATH
+# $PROJECT_BASE_PATH/env/bin/python manage.py migrate
+# $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
-# Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart monorbit-api-monolithic
+# # Configure supervisor
+# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# supervisorctl reread
+# supervisorctl update
+# supervisorctl restart monorbit-api-monolithic
 
-# Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-systemctl restart nginx.service
+# # Configure nginx
+# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+# rm /etc/nginx/sites-enabled/default
+# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+# systemctl restart nginx.service
 
 echo "DONE! :)"
