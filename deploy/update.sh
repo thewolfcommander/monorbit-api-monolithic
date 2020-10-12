40ec56f2db547d7a52e738ef5b413b0002921f8c
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-12 18:34:17 +0530

    Added Image upload feature, connected RDS

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,7 +4,8 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/monorbit-api-monolithic'
 
-echo "thewolfcommander" | echo "Billionaire2201" | read -p sudo git pull
+# echo "thewolfcommander" | echo "Billionaire2201" | read -p 
+sudo git pull
 # $PROJECT_BASE_PATH/env/bin/python manage.py makemigrations
 sudo $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 sudo $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
