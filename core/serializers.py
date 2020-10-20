9647302ae802dfcc26dc176a221b2846f5db3fd9
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2020-10-21 03:10:41 +0530

    Fixed bug in Creating Support Tickets

--- core/serializers.py
+++ core/serializers.py
@@ -9,86 +9,76 @@ from orders.serializers import OrderDetailSerializer
 class TipToGrowSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipToGrow
-        fields = [
-            'id',
-            'tip',
-            'upvotes',
-            'downvotes',
-            'active',
-            'added',
-            'updated'
-        ]
+        fields = ["id", "tip", "upvotes", "downvotes", "active", "added", "updated"]
 
     def updated(self, instance, validated_data):
-        instance.tip = validated_data.get('tip', instance.tip)
-        instance.upvotes = validated_data.get('upvotes', instance.upvotes)
-        instance.downvotes = validated_data.get('downvotes', instance.downvotes)
-        instance.active = validated_data.get('active', instance.active)
+        instance.tip = validated_data.get("tip", instance.tip)
+        instance.upvotes = validated_data.get("upvotes", instance.upvotes)
+        instance.downvotes = validated_data.get("downvotes", instance.downvotes)
+        instance.active = validated_data.get("active", instance.active)
         instance.save()
         return instance
 
-    
+
 class EmailSentToUsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmailSentToUsers
         fields = [
-            'id',
-            'email_type',
-            'sent_from_ip_address',
-            'email_sent_from',
-            'email_sent_to',
-            'subject',
-            'message',
-            'sent_on',
-            'is_success'
+            "id",
+            "email_type",
+            "sent_from_ip_address",
+            "email_sent_from",
+            "email_sent_to",
+            "subject",
+            "message",
+            "sent_on",
+            "is_success",
         ]
 
-    
+
 class UserLoginActivitySerializer(serializers.ModelSerializer):
     user = UserMiniSerializer(read_only=True)
+
     class Meta:
         model = UserLoginActivity
         fields = [
-            'id',
-            'user',
-            'ip_address',
-            'os_platform',
-            'browser',
-            'is_logged_from_mobile',
-            'is_logged_from_web',
-            'timestamp'
+            "id",
+            "user",
+            "ip_address",
+            "os_platform",
+            "browser",
+            "is_logged_from_mobile",
+            "is_logged_from_web",
+            "timestamp",
         ]
 
-    
+
 class UserDeviceRegistrationSerializer(serializers.ModelSerializer):
     user = UserMiniSerializer(read_only=True)
+
     class Meta:
         model = UserDeviceRegistration
         fields = [
-            'id',
-            'user',
-            'device_type',
-            'operating_system',
-            'browser',
-            'ip_addresss',
-            'lat',
-            'lng',
-            'device_language',
-            'user_agent',
-            'is_app',
-            'is_browser',
-            'timestamp'
+            "id",
+            "user",
+            "device_type",
+            "operating_system",
+            "browser",
+            "ip_addresss",
+            "lat",
+            "lng",
+            "device_language",
+            "user_agent",
+            "is_app",
+            "is_browser",
+            "timestamp",
         ]
 
-    
+
 class NetworkOrderSerializer(serializers.ModelSerializer):
     network = ShowNetworkSerializer(read_only=True)
     order = OrderDetailSerializer(read_only=True)
+
     class Meta:
         model = NetworkOrder
-        fields = [
-            'id',
-            'network',
-            'order',
-            'timestamp'
-        ]
+        fields = ["id", "network", "order", "timestamp"]
