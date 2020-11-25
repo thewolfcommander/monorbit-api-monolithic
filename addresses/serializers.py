bb5b716d424df31c5a6951f5c5f194c4b35a72cf
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-11-25 17:42:50 +0530

    Fixed Bug in Making One address default=True and other default=False

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -31,6 +31,20 @@ class AddressCreateSerializer(serializers.ModelSerializer):
             'updated'
         ]
 
+    def create(self, validated_data):
+        is_default = validated_data.get('is_default', False)
+        user = validated_data.pop('user')
+
+        if is_default:
+            queryset = Address.objects.filter(user=self.context['request'].user)
+            if queryset.exists():
+                for ad in queryset:
+                    ad.is_default = False
+                    ad.save()
+        
+        instance = Address.objects.create(**validated_data, user=self.context['request'].user)
+        return instance
+
     
 class AddressShowSerializer(serializers.ModelSerializer):
     user = UserMiniSerializer(read_only=True)
@@ -57,3 +71,29 @@ class AddressShowSerializer(serializers.ModelSerializer):
             'created',
             'updated'
         ]
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.alt_name = validated_data.get('alt_name', instance.alt_name)
+        instance.address_1 = validated_data.get('address_1', instance.address_1)
+        instance.address_2 = validated_data.get('address_2', instance.address_2)
+        instance.landmark = validated_data.get('landmark', instance.landmark)
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+        instance.country = validated_data.get('country', instance.country)
+        instance.pincode = validated_data.get('pincode', instance.pincode)
+        instance.alt_phone = validated_data.get('alt_phone', instance.alt_phone)
+        instance.lat = validated_data.get('lat', instance.lat)
+        instance.lng = validated_data.get('lng', instance.lng)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.is_archived = validated_data.get('is_archived', instance.is_archived)
+        
+        is_default = validated_data.get('is_default', False)
+
+        if is_default:
+            for ad in Address.objects.filter(user=instance.user):
+                ad.is_default=False
+                ad.save()
+            instance.is_default = True
+        instance.save()
+        return instance
--- monorbit/settings.py
+++ monorbit/settings.py
@@ -107,24 +107,24 @@ WSGI_APPLICATION = 'monorbit.wsgi.application'
 # host = config('SERVER_HOST', True)
 
 # if host:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-    
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('AWS_RDS_DB_NAME', 'AWS_RDS_DB_NAME'),
-        'USER': config('AWS_RDS_USERNAME', 'AWS_RDS_USERNAME'),
-        'PASSWORD': config('AWS_RDS_PASSWORD', 'AWS_RDS_PASSWORD'),
-        'HOST': config('AWS_RDS_HOST', 'AWS_RDS_HOST'),
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# else:
+    
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('AWS_RDS_DB_NAME', 'AWS_RDS_DB_NAME'),
+#         'USER': config('AWS_RDS_USERNAME', 'AWS_RDS_USERNAME'),
+#         'PASSWORD': config('AWS_RDS_PASSWORD', 'AWS_RDS_PASSWORD'),
+#         'HOST': config('AWS_RDS_HOST', 'AWS_RDS_HOST'),
+#         'PORT': 5432,
+#     }
+# }
 
 # add this below the database configuration
 import dj_database_url
