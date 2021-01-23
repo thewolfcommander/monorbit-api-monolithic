670c989d0258de1e8b4c957405f892baff0e58fc
HemendraKhatik <hemendrakhatik010698@gmail.com>
2021-01-23 14:22:26 +0530

    product_catalog permissions completed

--- product_catalog/permissions.py
+++ product_catalog/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from .models import ProductImage,Product
 
 
 class IsSubPartOwner(permissions.BasePermission):
@@ -13,11 +14,50 @@ class IsSubPartDetailOwner(permissions.BasePermission):
         elif request.method=="GET":
             return request.user.is_authenticated    
 
+class IsProductOwnersVariant(permissions.BasePermission):
+    """
+    Permission for Image, Video, Tag, Size, color, Specification and Extra of Product. 
+    If Product's User is request user then they create these otherwise not.
+    """
+
+    message="If Product user is requested user then only variant(image,video,document,tag) will create."
+    def has_permission(self,request,view):
+        product_id = request.data["product"]
+        print(product_id)
+        product = Product.objects.get(id=product_id)
+        return product.network.user == request.user
+
+     
+
+class IsProductOwnersVariantDetail(permissions.BasePermission):
+    """
+    Permission for Image, Video, Tag, Size, color, Specification and Extra of Product. 
+    If Product's User is request user then they update these otherwise not.
+    """
+    message="You are not owner of this object."
+    def has_object_permission(self,request,view,obj):
+        if request.method in ["PUT","PATCH","DELETE"]:
+            return obj.product.network.user == request.user
+        elif request.method=="GET":
+            return request.user.is_authenticated
 
 
 class IsSubSubPartOwner(permissions.BasePermission):
     def has_object_permission(self,request,view,obj):
         return obj.category.network.user == request.user
+
+class IsReviewOwner(permissions.BasePermission):
+    """
+    Review Owner can update and delete the review.
+    """
+
+    message="Only review owner can update review"
+    def has_object_permission(self,request,view,obj):
+        if request.method in ["PUT","PATCH","DELETE"]:
+            return obj.by == request.user
+        elif request.method=="GET":
+            return request.user
+        
 
 
 class  ProductAdminPermission(permissions.BasePermission):
