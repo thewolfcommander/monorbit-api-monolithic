71c6c38d4cd7fe8f4c01ab251e8206fe8ba3fddd
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-02-02 15:59:14 +0530

    Starting the deployment on heroku

--- monorbit/settings.py
+++ monorbit/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', True)
+DEBUG = False
 
 # ALLOWED_HOSTS = ['monorbit-alpha.herokuapp.com', 'localhost', '127.0.0.1', '34.212.227.245']
 ALLOWED_HOSTS = ['*']
