bb93b1ed59b98cff4be59e9944ce3cb1eb8a6fe0
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-02-05 13:54:47 +0530

    deploying on heroku

--- monorbit/utils/upload.py
+++ monorbit/utils/upload.py
@@ -41,7 +41,7 @@ def upload_file(file, filetype):
         # response = s3_client.upload_file(file, BUCKET_NAME, 'hello.png')
 
         with open(file, "rb") as f:
-            response = s3_client.upload_fileobj(file, BUCKET_NAME, '{}/{}/{}/{}/{}/{}'.format(filetype, today.year, today.month, today.day, today.hour, filename))
+            response = s3_client.upload_fileobj(file, BUCKET_NAME, '{}/{}/{}/{}/{}/{}'.format(filetype, today.year, today.month, today.day, today.hour, filename),ExtraArgs={'ContentType': 'text/x-python'})
         url = 'https://{}.s3.{}.amazonaws.com/{}/{}/{}/{}/{}/{}'.format(str(BUCKET_NAME), str(REGION), str(filetype), str(today.year), str(today.month), str(today.day), str(today.hour), str(filename))
         return url
     except ClientError as e:
